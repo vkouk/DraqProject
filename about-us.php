77a90a0de141b1503858aa871d66a5e220fe4b15
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-05 12:58:00 +0200

    Fixed html code for about-us page.

--- about-us.php
+++ about-us.php
@@ -1,56 +1,66 @@
 <!-- 2/3/2017 Bill:Made AboutUs Page. -->
 <div class="row">
-	<div class="col-md-12 team-info">
-		<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-		<ul class="team-list">
-			<p>Petros Trokas - <span>Project Manager</span></p>
-            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
-        </ul>
+	<div class="col-md-12">
+		<div class="team-info">
+			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<ul class="team-list">
+				<p>Petros Trokas - <span>Project Manager</span></p>
+	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
+	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+        	</ul>
+		</div>
 	</div>
 </div> <!-- row -->
 <div class="row">
-	<div class="col-md-6 team-info">
-		<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-		<ul class="team-list">
-			<p>Alex Ioannou - <span>Tester</span></p>
-            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
-        </ul>
+	<div class="col-md-6">
+		<div class="team-info">
+			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<ul class="team-list">
+				<p>Alex Ioannou - <span>Tester</span></p>
+	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
+	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+        	</ul>
+		</div>
 	</div>
-	<div class="col-md-6 team-info">
-		<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-		<ul class="team-list">
-			<p>Stauros - <span>Front End Developer</span></p>
-            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
-        </ul>
+	<div class="col-md-6">
+		<div class="team-info">
+			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<ul class="team-list">
+				<p>Stauros - <span>Front End Developer</span></p>
+	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
+	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+        	</ul>
+		</div>
 	</div>
 </div> <!-- row -->
 <div class="row">
-	<div class="col-md-6 team-info">
-		<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-		<ul class="team-list">
-			<p>Athanasia Oikonomou - <span>Back End Developer</span></p>
-            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
-        </ul>
+	<div class="col-md-6">
+		<div class="team-info">
+			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<ul class="team-list">
+				<p>Athanasia Oikonomou - <span>Back End Developer</span></p>
+	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
+	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+        	</ul>
+		</div>
 	</div>
-	<div class="col-md-6 team-info">
-		<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-		<ul class="team-list">
-			<p>Vasilis Koukoutis - <span>Front End Developer</span></p>
-            <li>
-            	<a href="#" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-            	<a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-            	<a href="#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
-            </li>
-            <li>
-            	<p>
-            		Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            	</p>
-            </li>
-        </ul>
+	<div class="col-md-6">
+		<div class="team-info">
+			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<ul class="team-list">
+				<p>Vasilis Koukoutis - <span>Front End Developer</span></p>
+	            <li>
+	            	<a href="#" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+	            	<a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+	            	<a href="#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
+	            </li>
+	            <li>
+	            	<p>
+	            		Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	            	</p>
+	            </li>
+	        </ul>
+		</div>
 	</div>
 </div> <!-- row -->
