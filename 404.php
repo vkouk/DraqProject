d879bdad439aabc4f615b83981d63c382e996279
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-08 23:35:01 +0200

    404.php error page added.

---
+++ 404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>404 Error Page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="error-page">
+				<a href="index.php"><img class="img-responsive" src="images/logo.png" alt="Draq Logo"></a>
+				<div class="col-sm-12 col-md-10 col-md-offset-1">
+					<p>404 Page Not Found</p>
+					Please go back to our <a href="index.php">Home Page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
