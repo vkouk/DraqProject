14e4537b3c85de8cc33139a77324ad5a5b5a546f
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-08 22:52:50 +0200

    Updated about-us.More to come.

--- about-us.php
+++ about-us.php
@@ -2,11 +2,20 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="team-info">
-			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<img src="images/ptrokas-photo.jpg" alt="Petros Trokas">
 			<ul class="team-list">
 				<p>Petros Trokas - <span>Project Manager</span></p>
-	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+	            <li>
+	            	<a href="https://twitter.com/PetrosTrk" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+	            	<a href="mailto:p.trokas@mc-class.gr" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
+	            </li>
+	            <li>
+	            	<p>
+	            		Networks & Security
+						IT Manager in Customs of Elefsina
+	            	</p>
+	            </li>
         	</ul>
 		</div>
 	</div>
@@ -14,11 +23,19 @@
 <div class="row">
 	<div class="col-md-6">
 		<div class="team-info">
-			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
+			<img src="images/ialex-photo.jpg" alt="Alex Ioannou">
 			<ul class="team-list">
 				<p>Alex Ioannou - <span>Tester</span></p>
-	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+	            <li>
+	            	<a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+	            	<a href="mailto:#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
+	            </li>
+	            <li>
+	            	<p>
+	            		Networks & Security
+	            	</p>
+	            </li>
         	</ul>
 		</div>
 	</div>
@@ -50,14 +67,14 @@
 			<ul class="team-list">
 				<p>Vasilis Koukoutis - <span>Front End Developer</span></p>
 	            <li>
-	            	<a href="#" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-	            	<a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-	            	<a href="#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+	            	<a href="https://www.linkedin.com/in/vasilis-koukoutis-82807297/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+	            	<a href="https://twitter.com/vkoukoutis" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+	            	<a href="mailto:v.koukoutis@mc-class.gr" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
 	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
 	            </li>
 	            <li>
 	            	<p>
-	            		Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	            		Studying BSc (Hons) in Computer Science @ University Of Derby / Love Computers & Programmimg / Enjoy Hanging Out / Love Tasting Foreign Kitchens.
 	            	</p>
 	            </li>
 	        </ul>
