1c9a2f4ab94174cb7286fef9f6282e54d1a4a44e
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-08 23:10:22 +0200

    Changed whole login page.

--- login.php
+++ login.php
@@ -10,41 +10,28 @@
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 </head>
 <body>
-	<header>
-		<a href="index.php"><img class="img-responsive" src="images/logo.png" alt="Draq Logo"></a>
-	</header>
-
-	<div class="login-main">
-		<div class="page">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<form class="form-horizontal login-form" method="post" action="login.php">
-							<div class="form-group">
-								<label for="username" class="col-sm-2 control-label">Username</label>
-								<div class="col-sm-10 input-group">
-								 	<div class="input-group-addon"><i class="fa fa-user-o" aria-hidden="true"></i></div>
-									<input type="name" id="username">
-								</div>
-							</div>
-							<div class="form-group">
-							    <label for="pswd" class="col-sm-2 control-label">Password</label>
-							    <div class="col-sm-10 input-group">
-								    <div class="input-group-addon"><i class="fa fa-key" aria-hidden="true"></i></div>
-								    <input type="password" id="pswd">
-							    </div>
-  							</div>
-  							<div class="form-group">
-							    <div class="col-sm-offset-2 col-sm-10">
-							      <button type="submit" class="btn btn-lg btn-login"><i class="fa fa-check" aria-hidden="true"></i> Sign in</button>
-							    </div>
-  							</div>
-						</form> <!-- form -->
-					</div>
-				</div> <!-- row -->
-			</div> <!-- container -->
-		</div> <!-- page -->
-	</div> <!-- login-main -->
+	<div class="container">
+		<div class="row">
+			<div class="login-main">
+				<a href="index.php"><img class="img-responsive" src="images/logo.png" alt="Draq Logo"></a>
+				<div class="col-sm-12 col-md-10 col-md-offset-1">
+					<form class="login-form" method="post" action="login.php">
+						<div class="form-group input-group">
+					        <div class="input-group-addon"><i class="fa fa-user-o" aria-hidden="true"></i></div>
+					        <input class="form-control" id="username" type="text" name='username' placeholder="username"/>          
+	          			</div>
+						<div class="form-group input-group">
+					        <div class="input-group-addon"><i class="fa fa-key" aria-hidden="true"></i></div>
+					        <input class="form-control" id="pswd" type="password" name='pswd' placeholder="password"/>          
+	          			</div>
+	  					<div class="form-group">
+	            			<button type="submit" class="btn btn-block btn-login"><i class="fa fa-check" aria-hidden="true"></i> Sign in</button>
+	          			</div>
+					</form> <!-- form -->
+				</div> <!-- col-sm-12 col-md-10 col-md-offset-1 -->
+			</div> <!-- login-main -->
+		</div> <!-- row -->
+	</div> <!-- container -->
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
