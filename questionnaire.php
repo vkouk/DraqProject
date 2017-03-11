708d231063e5c6a1f6c2e29baa944bda5ab061ec
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-11 14:25:32 +0200

    Fixed responsive style for questionnaire.

--- questionnaire.php
+++ questionnaire.php
@@ -1,12 +1,12 @@
 <div class="row">
-	<div class="col-md-6">
+	<div class="col-sm-6 col-md-6">
 		<a href="index.php?page=about-questionnaire.php">
 			<div class="caption">
 				<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quo esse veritatis, nobis doloremque minus vitae.</span>
 			</div>
 		</a>
 	</div> <!-- col-md-6 -->
-	<div class="col-md-6">
+	<div class="col-sm-6 col-md-6">
 		<a href="index.php?page=do-test.php">
 			<div class="caption">
 				<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quo esse veritatis, nobis doloremque minus vitae.</span>
