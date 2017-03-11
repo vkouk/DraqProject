18fad58644077f8bde33b8affe6947cef98e4b7f
Unknown <saskantzis@gmail.com>
2017-03-11 09:43:38 +0200

    added style definitions for useful links page

--- useful-links.php
+++ useful-links.php
@@ -1,3 +1,11 @@
-<div class="row">
+<div class="rectangle">
+    <h3 align="center">Useful Links</h3>
+    <div class="links">
+        <a href="https://www.diabetes.org.uk/">Diabetes United Kingdom</a><br>
+        <a href="https://www.diabetes.ca/">Canadian Diabetes Association</a><br>
+        <a href="http://www.diabetes.org/">American Diabetes Association</a><br>
+        <a href="http://www.ede.gr/">Ελληνική Διαβητολογική Εταιρεία</a><br>
+        <a href="http://www.onmed.gr">onmed.gr</a>
+    </div>
 	
 </div> <!-- row -->
