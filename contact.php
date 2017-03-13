2749ca50e12c482d10605021f50f5fef82b3053f
Unknown <saskantzis@gmail.com>
2017-03-13 23:53:36 +0200

    basic formatting of the page

--- contact.php
+++ contact.php
@@ -1,3 +1,20 @@
 <div class="row">
+    <div class="col-xs-6">
+        <p>
+            If you wish to contact us, send us an email at emailadress@domain.com.
+        </p>
+
+    </div>
+    <div class="col-xs-6">
+        <form action="" method="">
+            <p>
+            Name: <input type="text"><br>
+            Email: <input type="text"><br>
+            Your Message: <input type="text"><br>
+            <input type="submit">
+            </p>
+        </form>
+
+    </div>
 	
 </div> <!-- row -->
