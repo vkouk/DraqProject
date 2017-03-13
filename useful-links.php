ed490f2943e1fa7cde744ae8845145467c3c835a
Unknown <saskantzis@gmail.com>
2017-03-13 22:26:54 +0200

    format changes to useful links / what is diabetes pages

--- useful-links.php
+++ useful-links.php
@@ -1,11 +1,14 @@
 <div class="rectangle">
     <h3 align="center">Useful Links</h3>
     <div class="links">
-        <a href="https://www.diabetes.org.uk/">Diabetes United Kingdom</a><br>
-        <a href="https://www.diabetes.ca/">Canadian Diabetes Association</a><br>
-        <a href="http://www.diabetes.org/">American Diabetes Association</a><br>
-        <a href="http://www.ede.gr/">Ελληνική Διαβητολογική Εταιρεία</a><br>
-        <a href="http://www.onmed.gr">onmed.gr</a>
+        <ul class="list-unstyled">
+            <li><a href="https://www.diabetes.org.uk/">Diabetes United Kingdom</a></li>
+            <li><a href="https://www.diabetes.ca/">Canadian Diabetes Association</a></li>
+            <li><a href="http://www.diabetes.org/">American Diabetes Association</a></li>
+            <li><a href="http://www.ede.gr/">Ελληνική Διαβητολογική Εταιρεία</a></li>
+            <li><a href="http://www.onmed.gr">onmed.gr</a></li>
+            <li><a href="http://www.webmd.com/diabetes">WebMD Diabetes</a></li>
+        </ul>
     </div>
 	
 </div> <!-- row -->
