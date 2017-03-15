d68285b60554fb5e80b54889f1c17bab4fe81b12
Unknown <saskantzis@gmail.com>
2017-03-15 15:46:31 +0200

    added greek "living-with" page

--- living-with.php
+++ living-with.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-6 col-lg-4">
+    <div class="col-md-6 col-lg-4 diabetes-page">
         <h4>
 
         </h4>
@@ -7,6 +7,6 @@
             <a href="about-diabetes.php"></a>
             <a href="prediabetes.php"></a>
         </p>
-    </div>div>
+    </div>
 	
 </div> <!-- row -->
