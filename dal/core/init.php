165527bc78befd329bfc440ddb138e8e12e9312b
Nassia Oikonomou <athanoik@gmail.com>
2017-04-02 20:58:52 +0300

    Inserted some dal folders
    Inside dal folders, there is the init.php with all the information for the database. Will  be called inside the test and admin file. Inside the classes folders, are the some classes for session, database, user and input (GET or POST).

---
+++ dal/core/init.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host'=>'db24.papaki.gr:3306',
+        'username'=>'n9126_diabadmin',
+        'password'=>'diab9#ad2Cq5&',
+        'db'=>'n91264atha_diabetesdb'
+    ),
+
+    'session'=> array(
+        'session_name'=>'user',
+        'token_name'=>'token'
+    )
+);
+
+
+spl_autoload_register(function($class){
+    require_once 'classes/'.$class.'.php';
+});
+
+require_once 'functions/sanitize.php';
+
+if(Cookie::exists(config::get('remember/cookie_name')) && !Session::exists(config::get('session/session_name'))){
+    $hash = Cookie::get(config::get('remember/cookie_name'));
+    $hashCheck = DB::getInstance()->get('user_session', array('hash','=', $hash));
+
+    if($hashCheck->count()){
+
+        $user = new User($hashCheck->first()->user_id);
+        $user->login();
+    }
+
+}
+
+
+?>
