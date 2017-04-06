9d0e939517970cf8a6cfb3e2c35377d2def076a9
Nassia Oikonomou <athanoik@gmail.com>
2017-04-06 13:58:41 +0300

    Created login to dashboard
    Finished creating login.php. Created logout.php along with their functionality. Made a small navbar for a logout button in dashboard. Added a 403 http error for unauthorized access.

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'core/init.php';
 /**
 * Created by PhpStorm.
 * User: Dragoness_crysta
@@ -7,6 +7,13 @@
 * Time: 1:02
 * Under construction...
 */
+
+$user = new User();
+
+if(!$user->isLoggedIn())
+{
+    header("Location:403.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,22 +24,35 @@
             text-align:center;
             margin-top:10%;
         }
-        h1{
-            font-size:4em;
+        h1.centered{
+            font-size:5em;
         }
         
         body{
             background: url("images/da-background.jpg") no-repeat center center fixed;
             background-size:cover;
         }
+
+        .size2{
+            font-size:2em;
+        }
     </style>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 </head>
 
 <body>
 <div class="row">
+    <nav class="navbar">
+        <div class="container-fluid">
+
+        <ul class="nav navbar-nav navbar-right">
+            <li style="margin-right:50px;"class="size2"><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
+        </ul>
+        </div>
+    </nav>
     <h1 class="centered animated flipInX">Dashboard Under construction.</h1>
 </div>
 </body>
