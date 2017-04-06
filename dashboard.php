c984799eee313c11dde3db2a0446e34ef314335b
Nassia Oikonomou <athanoik@gmail.com>
2017-04-06 12:41:39 +0300

    Simple dashboard
    Mad a dashboard under construction page for further development.

--- dashboard.php
+++ dashboard.php
@@ -1,22 +1,40 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: Dragoness_crysta
- * Date: 6/4/2017
- * Time: 1:02
- */
+* Created by PhpStorm.
+* User: Dragoness_crysta
+* Date: 6/4/2017
+* Time: 1:02
+* Under construction...
+*/
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
+    <style>
+        .centered{
+            text-align:center;
+            margin-top:10%;
+        }
+        h1{
+            font-size:4em;
+        }
+        
+        body{
+            background: url("images/da-background.jpg") no-repeat center center fixed;
+            background-size:cover;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
 </head>
 
 <body>
-
-<h1 style="vertical-align:center; text-align:justify;">This is a future dashboard</h1>
-
+<div class="row">
+    <h1 class="centered animated flipInX">Dashboard Under construction.</h1>
+</div>
 </body>
 
 </html>
