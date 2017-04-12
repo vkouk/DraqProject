47c4703163d0a777d944ceb5d5995bdfd12c0723
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-04-13 00:44:45 +0300

    Questionnaire Refactor 1/5 /2 - DONE

--- do-test.php
+++ do-test.php
@@ -134,11 +134,7 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question1.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                    </div>
-                </div> <!-- questionnaire-footer -->
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="gender" data-id="gender2">
                 <h3 id="questionnaire-header">2.Please specify your gender.</h3>
@@ -149,12 +145,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question2.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                    </div>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="age" data-id="age3">
                 <h3 id="questionnaire-header">3.Please specify your age.</h3>
@@ -167,12 +159,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                         <img class="img-responsive" src="images/quest-icons/question3.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                    </div>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="weight" data-id="weight4">
                 <h3 id="questionnaire-header">4.How much do you weight?</h3>
@@ -182,11 +170,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question4.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="height" data-id="height5">
                 <h3 id="questionnaire-header">5.What is your height?</h3>
@@ -196,11 +181,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question5.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="bmi" data-id="bmi6">
                 <h3 id="questionnaire-header">6.Bmi</h3>
@@ -210,11 +192,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question6.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="waist" data-id="waist7">
                 <h3 id="questionnaire-header">7.What is your waist size?</h3>
@@ -224,11 +203,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question7.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="activity" data-id="activity8">
                 <h3 id="questionnaire-header">8.Do you exercise often?</h3>
@@ -239,11 +215,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question8.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="food_habit" data-id="food_habit9">
                 <h3 id="questionnaire-header">9.How often do you eat fruits or vegetables?</h3>
@@ -254,11 +227,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question9.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="hbp" data-id="hbp10">
                 <h3 id="questionnaire-header">10.Have you taken high blood preasure medication?</h3>
@@ -269,11 +239,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question10.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="blood_sugar" data-id="blood_sugar11">
                 <h3 id="questionnaire-header">11.Have you ever had high blood sugar levels?</h3>
@@ -284,11 +251,8 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question11.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="family_history" data-id="family_history12">
                 <h3 id="questionnaire-header">12.Do you have a close relative with type 1 or 2 diabetes?</h3>
@@ -300,22 +264,16 @@ if(input::exists()){
                 <div class="col-md-12 col-lg-6">
                     <img class="img-responsive" src="images/quest-icons/question12.png" alt="">
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12">
-                        <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
-                        <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="next-quest"><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></a>
             </fieldset>
             <fieldset data-name="risk" data-id="risk13">
                 <h3 id="questionnaire-header">Your result</h3>
                 <div class="col-md-12 col-lg-12">
                     <p>todo</p>
                 </div> <!-- col-md-12 col-lg-6 -->
-                <div class="questionnaire-footer">
-                    <div class="col-xs-12 col-md-12 col-lg-12"> 
-                        <a class="finish-quest"><i class="fa fa-check-circle fa-2x" aria-hidden="true"></i></a>
-                    </div>
-                </div> <!-- questionnaire-footer -->
+                <a class="prev-quest"><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></a>
+                <a class="finish-quest"><i class="fa fa-check-circle fa-2x" aria-hidden="true"></i></a>
             </fieldset>
         </form> <!-- questionnaire -->
     </div> <!-- col-md-12 col-lg-12 -->
