822c21e9d342a15a1de6fb56d925aaf32865d900
Nassia Oikonomou <athanoik@gmail.com>
2017-05-03 13:20:04 +0300

    Fixed english and greek versions.
    Fixed english about us page, result.php maps both in greek and english.

--- about-us.php
+++ about-us.php
@@ -12,8 +12,7 @@
 	            </li>
 	            <li>
 	            	<p>
-	            		Networks & Security
-						IT Manager in Customs of Elefsina
+	            		I am working as an IT Manager in the Customs of Elefsina in Athens Greece. I am currently studying Networks and Security in the University of Derby through Mediterranean College.
 	            	</p>
 	            </li>
         	</ul>
@@ -24,15 +23,15 @@
 	<div class="col-md-6">
 		<div class="team-info">
 			<img src="images/ialex-photo.jpg" alt="Alex Ioannou">
-			<ul class="team-list">
-				<p>Alex Ioannou - <span>Web Designer & Editor</span></p>
+			<ul class="team-list team-h">
+				<p>Alexandros Ioannou - <span>Web Designer & Editor</span></p>
 	            <li>
 	            	<a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 	            	<a href="mailto:#" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
 	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
 	            </li>
 	            <li>
-	            	<p>
+	            	<p class="team-p">
 	            		"There is no place like 127.0.0.1"
 						Studying Networks and Security, currently on the second year. Avid coffee addict and gaming enthusiast. Planning on working abroad after I get my bachelor degree.
 	            	</p>
@@ -43,10 +42,13 @@
 	<div class="col-md-6">
 		<div class="team-info">
 			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-			<ul class="team-list">
-				<p>Stauros - <span>Front End Developer</span></p>
-	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
+			<ul class="team-list team-h">
+				<p>Stavros Skantzis - <span>Front End Developer</span></p>
+	            <li><a href="mailto:s.skantzis@mc-class.gr" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+                <p class="team-p">
+                    I am currently studying Computer Science in University of Derby through Mediterranean college.
+                    Getting interested in Computers, was the best idea of my life - after veganism and video games.
+                </p>
         	</ul>
 		</div>
 	</div>
@@ -55,30 +57,38 @@
 	<div class="col-md-6">
 		<div class="team-info">
 			<img src="images/noikonomou-photo.jpg" alt="Athanasia Oikonomou">
-			<ul class="team-list">
-				<p>Athanasia Oikonomou - <span>Back End Developer</span></p>
-	            <li><a href="https://twitter.com/adamconnor">@adamconnor</a></li>
-	            <li><a href="http://adamconnor.com/">adamconnor.com</a></li>
-        	</ul>
+            <ul class="team-list team-h">
+                <p>Athanassia Oikonomou - <span>Full stack Developer</span></p>
+                <li>
+                    <a href="mailto:athan.oikonomou@mc-class.gr"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+                </li>
+                <p class="team-p">
+                    If debugging is the process of removing software bugs, then programming must be the process of putting them in - Edsger Dijkstra.
+                    I am currently studying Computer Science in the University of Derby through Mediterranean College. The deeper I dive into the world of Computer Science, the more exciting it gets.
+                    My final goal: to write code that simply runs without errors.
+                </p>
+            </ul>
 		</div>
 	</div>
 	<div class="col-md-6">
 		<div class="team-info">
 			<img src="images/vkouk-photo.jpg" alt="Vasilis Koukoutis">
-			<ul class="team-list">
-				<p>Vasilis Koukoutis - <span>Front End Developer</span></p>
-	            <li>
-	            	<a href="https://www.linkedin.com/in/vasilis-koukoutis-82807297/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-	            	<a href="https://twitter.com/vkoukoutis" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-	            	<a href="mailto:v.koukoutis@mc-class.gr" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-	            	<a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
-	            </li>
-	            <li>
-	            	<p>
-	            		Studying BSc (Hons) in Computer Science @ University Of Derby / Love Computers & Programmimg / Enjoy Hanging Out / Love Tasting Foreign Kitchens.
-	            	</p>
-	            </li>
-	        </ul>
+            <ul class="team-list team-h">
+                <p>Vasilis Koukoutis - <span>Front End Developer</span></p>
+                <li>
+                    <a href="https://www.linkedin.com/in/vasilis-koukoutis-82807297/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/vkoukoutis" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+                    <a href="mailto:v.koukoutis@mc-class.gr" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                    <p class="team-p">
+                        I've been programming since I was sixteen, and the more I get involved it, the more I like it. I am currently studying BSc Computer Science in the University of
+                        Derby UK in Athens through Mediterranean College, in the second year.
+                        In my spare time I love trying different cuisines, hanging out with friends and last but not least, drink lots of beer.
+                    </p>
+                </li>
+            </ul>
 		</div>
 	</div>
 </div> <!-- row -->
--- result.php
+++ result.php
@@ -42,54 +42,90 @@ if ($results->isInSession()) {
         <h1>Your points are: <?php echo $results->ShowPoints(); ?></h1><br>
         <h1>Your risk is: <?php echo $results->risk(); ?></h1><br>
         <fieldset class="risk-alert result-1" data-name="result" data-id="result1">
-            <div class="col-md-6">
-                <span data-color="r1">< 7</span>
-            </div>
-            <div class="col-md-6">
-                <p id="result-text result-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus est
-                    iusto nam, nemo neque nobis numquam ullam velit.</p>
+            <div class="row">
+                <div class="col-md-2">
+                    <span data-color="r1">< 7</span>
+                </div>
+                <div class="col-md-4  risk-text">Low Risk</div>
+                <div class="col-md-6 ">
+                    <p id="result-text">It is estimated that 1 in 100 people already has type 2 diabetes or will develop it in the next 10 years.</p>
+                </div>
             </div>
         </fieldset>
         <fieldset class="risk-alert result-2" data-name="result" data-id="result2">
-            <div class="col-md-6">
-                <span data-color="r2"> 7 - 11</span>
-            </div>
-            <div class="col-md-6">
-                <p id="result-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus est iusto
-                    nam, nemo neque nobis numquam ullam velit.</p>
+            <div class="row">
+                <div class="col-md-2">
+                    <span data-color="r2"> 7 - 11</span>
+                </div>
+                <div class="col-md-4  risk-text">Slightly Increased Risk </div>
+                <div class="col-md-6">
+                    <p id="result-text">It is estimated that 1 in 25 people already has type 2 diabetes or will develop it in the next 10 years.</p>
+                </div>
             </div>
         </fieldset>
         <fieldset class="risk-alert result-3" data-name="result" data-id="result3">
-            <div class="col-md-6">
-                <span data-color="r3">12 - 14</span>
-            </div>
-            <div class="col-md-6">
-                <p id="result-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus est iusto
-                    nam, nemo neque nobis numquam ullam velit.</p>
+            <div class="row">
+                <div class="col-md-2">
+                    <span data-color="r3">12 - 14</span>
+                </div>
+                <div class="col-md-4  risk-text">Moderately Increased Risk </div>
+                <div class="col-md-6">
+                    <p id="result-text">It is estimated that 1 in 6 people already has type 2 diabetes or will develop it in the next 10 years.</p>
+                </div>
             </div>
         </fieldset>
         <fieldset class="risk-alert result-4" data-name="result" data-id="result4">
-            <div class="col-md-6">
-                <span data-color="r4">15 - 20</span>
-            </div>
-            <div class="col-md-6">
-                <p id="result-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus est iusto
-                    nam, nemo neque nobis numquam ullam velit.</p>
+            <div class="row">
+                <div class="col-md-2">
+                    <span data-color="r4">15 - 20</span>
+                </div>
+                <div class="col-md-4  risk-text">High Risk</div>
+                <div class="col-md-6">
+                    <p id="result-text">It is estimated that 1 in 3 people already has type 2 diabetes or will develop it in the next 10 years.</p>
+                </div>
             </div>
         </fieldset>
         <fieldset class="risk-alert result-5" data-name="result" data-id="result5">
-            <div class="col-md-6">
-                <span data-color="r5">21 - 26+</span>
-            </div>
-            <div class="col-md-6">
-                <p id="result-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus est iusto
-                    nam, nemo neque nobis numquam ullam velit.</p>
+            <div class="row">
+                <div class="col-md-2">
+                    <span data-color="r5">21 - 26+</span>
+                </div>
+                <div class="col-md-4  risk-text">Extremely High Risk</div>
+                <div class="col-md-6">
+                    <p id="result-text">It is estimated that 1 in 2 people already has type 2 diabetes or will develop it in the next 10 years.</p>
+                </div>
             </div>
         </fieldset>
-        <h2>If you wish to retake the test, click <a href="index.php?page=do-test.php">here</a></h2>
     </div>
 </div>
 
+<div class="row risk-info">
+    <p>In case you find yourselves in one of the high risk zones 3, 4, or 5,
+        <strong>you should contact immediately</strong> your personal physician or go
+        to the nearest hospital with a Diabetes center,
+        in order to undergo one of the following blood exams:</p>
+    <ul>
+        <li>Fasting Plasma Glucose Test</li>
+        <li>Hemoglobin A1c Test</li>
+    </ul>
+
+</div>
+
+<div class="row">
+    <div class="col-md-12 risk-info">
+        <p>In the following map you will find all the available diabetic centers in Greece.</p>
+    </div>
+    <div class="col-md-12">
+        <iframe src="https://www.google.com/maps/d/embed?mid=1SnfFXRiotlCaI1ZHJGPLLNo-e6Y" width="100%" height="700px"></iframe>
+    </div>
+    <div class="col-md-12 risk-info">
+        <p> In case you want to download the full list of the diabetic centers in greek, click <a target="_blank" href="http://www.galinos.org.gr/portal/pdf/%CE%94%CE%B9%CE%B1%CE%B2%CE%B7%CF%84%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CE%BA%CE%AC-%CE%95%CE%BD%CE%B4%CE%BF%CE%BA%CF%81%CE%B9%CE%BD%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CE%BA%CE%AC%20%CE%9A%CE%AD%CE%BD%CF%84%CF%81%CE%B1.pdf">here</a></p>
+    </div>
+</div>
+
+<div class="row risk-info">
+    <p>If you wish to retake the test click <a href="index.php?page=do-test.php">here</a></p>
+</div>
 
 <script type="text/javascript">
         var finalRiskFactor = <?php echo $results->risk(); ?>;
